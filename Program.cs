2f4755ec9e3cefae2a021fb881597cddad145783
Malu23 <malududa54@gmail.com>
2020-12-23 11:49:00 -0300

    Inserção de pastas essenciais

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace bootcamp_DDD
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
